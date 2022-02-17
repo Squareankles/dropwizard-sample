03c2885f3e9e3a0311e0456a21330c870d3b8485
n1559202 <michael.walsh01@liberty-it.co.uk>
2022-02-17 15:29:50 +0000

    Add validation test to resource test

--- src/main/java/com/example/resources/CustomerResource.java
+++ src/main/java/com/example/resources/CustomerResource.java
@@ -5,6 +5,8 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import com.example.api.CustomerResponse;
 import com.example.core.service.CustomerService;
 import com.google.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -22,7 +24,7 @@ public class CustomerResource {
   }
 
   @GET
-  public CustomerResponse get(@HeaderParam("user-id") String userId) {
+  public CustomerResponse get(@HeaderParam("user-id") @Valid @Size(min = 36, max = 36) String userId) {
     return this.customerService.get(userId);
   }
 
--- src/test/java/com/example/resources/CustomerResourceTest.java
+++ src/test/java/com/example/resources/CustomerResourceTest.java
@@ -1,5 +1,6 @@
 package com.example.resources;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -43,20 +44,43 @@ class CustomerResourceTest {
   private static final String MOBILE = "0569785215";
 
   @Test
-  void getTest() throws JSONException {
+  void getSuccess() throws JSONException {
     // Arrange
     CustomerResponse responseObject = new CustomerResponse(
         Collections.singletonList(new Customer(new NameFamily(F_NAME, L_NAME),
             new AddressFamily(LINE_1, LINE_2, CITY), new ContactFamily(LANDLINE, MOBILE))));
     when(customerService.get("8943e2d6-6e37-4045-8e21-746440645e81")).thenReturn(responseObject);
+
     // Act
     final Response response = EXT.target("/v1/customers").request()
         .header("user-id", "8943e2d6-6e37-4045-8e21-746440645e81").get();
 
     // Assert
     assertNotNull(response);
+    assertEquals(200, response.getStatus());
     JSONAssert.assertEquals(
         "{\"customers\":[{\"name\":{\"firstName\":\"fname\",\"lastName\":\"lname\"},\"address\":{\"line1\":\"line1\",\"line2\":\"line2\",\"city\":\"city\"},\"contact\":{\"mobile\":\"0569785215\"}}]}",
         response.readEntity(String.class), true);
+  }
+
+  @Test
+  void getValidationError() throws JSONException {
+    // Arrange
+    CustomerResponse responseObject = new CustomerResponse(
+        Collections.singletonList(new Customer(new NameFamily(F_NAME, L_NAME),
+            new AddressFamily(LINE_1, LINE_2, CITY), new ContactFamily(LANDLINE, MOBILE))));
+    when(customerService.get("8")).thenReturn(responseObject);
+
+    // Act
+    final Response response = EXT.target("/v1/customers").request()
+        .header("user-id", "8").get();
+
+    // Assert
+    assertNotNull(response);
+    assertEquals(400, response.getStatus());
+    JSONAssert.assertEquals("{\"errors\":[\"header user-id size must be between 36 and 36\"]}",
+        response.readEntity(String.class), true);
+
+
   }
 }
