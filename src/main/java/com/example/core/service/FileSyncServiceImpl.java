ae16bc586ad192df45dc5a541cdbf9c39f095dc1
n1559202 <michael.walsh01@liberty-it.co.uk>
2022-02-17 10:23:21 +0000

    Add completable futures

--- src/main/java/com/example/core/service/FileSyncServiceImpl.java
+++ src/main/java/com/example/core/service/FileSyncServiceImpl.java
@@ -4,6 +4,7 @@ import static com.example.db.entity.DataSyncStatus.OPENED;
 import static com.example.db.entity.DataSyncStatus.SUBMITTED;
 import static java.time.ZoneOffset.UTC;
 
+import com.codahale.metrics.annotation.Timed;
 import com.example.client.FileSyncClient;
 import com.example.core.model.StatusValidationHolder;
 import com.example.core.validator.Validator;
@@ -16,8 +17,12 @@ import com.google.inject.Inject;
 import java.io.File;
 import java.time.ZonedDateTime;
 import java.util.UUID;
-import ru.vyarus.guicey.jdbi3.tx.InTransaction;
+import java.util.concurrent.CompletableFuture;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import ru.vyarus.guicey.jdbi3.tx.TransactionTemplate;
 
+@Slf4j
 public class FileSyncServiceImpl implements FileSyncService {
 
   private final FileSyncClient fileSyncClient;
@@ -30,45 +35,62 @@ public class FileSyncServiceImpl implements FileSyncService {
 
   private final FileSyncDao fileSyncDao;
 
+  private final TransactionTemplate transactionTemplate;
+
   @Inject
   public FileSyncServiceImpl(FileSyncClient fileSyncClient,
       SyncOperationStatusDao syncOperationStatusDao,
       Validator<StatusValidationHolder> statusValidator,
-      FileSyncProducer fileSyncProducer, FileSyncDao fileSyncDao) {
+      FileSyncProducer fileSyncProducer, FileSyncDao fileSyncDao,
+      TransactionTemplate transactionTemplate) {
     this.fileSyncClient = fileSyncClient;
     this.syncOperationStatusDao = syncOperationStatusDao;
     this.statusValidator = statusValidator;
     this.fileSyncProducer = fileSyncProducer;
     this.fileSyncDao = fileSyncDao;
+    this.transactionTemplate = transactionTemplate;
   }
 
-  @InTransaction
+  @Timed
+  @SneakyThrows
   @Override
   public void upload(UUID operationId, File file, String fileName) {
-
+    log.info("Got here 1");
     String fileKey = null;
+    final String key;
     final UUID uploadKey = UUID.randomUUID();
     final ZonedDateTime now = ZonedDateTime.now(UTC);
 
     try {
+
       // Check the status of the operation
-      OperationStatus operationStatus = this.syncOperationStatusDao.get(operationId.toString());
+
+      CompletableFuture<OperationStatus> opStatusFuture = CompletableFuture
+          .supplyAsync(() -> this.syncOperationStatusDao.get(operationId.toString()));
+      CompletableFuture<String> fileSyncFuture = CompletableFuture
+          .supplyAsync(() -> this.fileSyncClient.upload(file, uploadKey, fileName));
+
+      CompletableFuture.allOf(fileSyncFuture, opStatusFuture).get();
+      OperationStatus operationStatus = opStatusFuture.get();
+      fileKey = fileSyncFuture.get();
+      key = fileKey;
+      String fileLocation = this.fileSyncClient.getUrl(fileKey);
 
       this.statusValidator
           .validate(new StatusValidationHolder(operationStatus.getStatus(), OPENED));
 
-      // Upload the file
-      fileKey = this.fileSyncClient.upload(file, uploadKey, fileName);
-      String fileLocation = this.fileSyncClient.getUrl(fileKey);
-      // Save the upload record
-      this.fileSyncDao.create(new FileSync(uploadKey, operationId, fileLocation, fileKey, now));
+      transactionTemplate.inTransaction((handle) -> {
+
+        // Save the upload record
+        this.fileSyncDao.create(new FileSync(uploadKey, operationId, fileLocation, key, now));
+        // Update the status
+        this.syncOperationStatusDao
+            .create(new OperationStatus(UUID.randomUUID(), operationId, SUBMITTED, now));
+        return null;
+      });
 
       // Send to kafka
       this.fileSyncProducer.send(operationId);
-
-      // Update the status
-      this.syncOperationStatusDao
-          .create(new OperationStatus(UUID.randomUUID(), operationId, SUBMITTED, now));
 
     } catch (Exception e) {
       if (fileKey != null) {
